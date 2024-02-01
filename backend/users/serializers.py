e2797d38264c0c1f667b933f5f10269d821fb8bb
Adibov <hesamadibi80@gmail.com>
2024-02-01 21:06:26 +0330

    fix: fix updating/creating user

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,3 +7,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name', 'phone', 'avatar', 'bio')
+        read_only_fields = ['id']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        if validated_data.get('password'):
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
