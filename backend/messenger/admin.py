6dda92ccc7774ae25cc05b2204af386a52e0fe28
mr-amirfazel <amirfazel45@gmail.com>
2024-08-27 16:04:52 +0330

    [fix]<app>: removed env files and unneeded directories

--- backend/messenger/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from messenger.models import Contact, Chat, ChatMembership, Message
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(Chat)
-admin.site.register(ChatMembership)
-admin.site.register(Message)
